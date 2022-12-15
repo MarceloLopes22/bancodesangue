9c9389442ba068c36f8664f35d0a92df273443b0
Marcelo Lopes <marcelo.filho@elo.inf.br>
2022-12-14 22:55:52 -0300

    IMC médio em cada faixa de idade de dez em dez anos: 0 a 10; 11 a 20; 21 a 30, etc. (IMC = peso / altura^2) Qual o percentual de obesos entre os homens e entre as mulheres? (É obeso quem tem IMC >30)

--- candidato/src/main/java/com/wk/candidato/servico/CandidatoServico.java
+++ candidato/src/main/java/com/wk/candidato/servico/CandidatoServico.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.function.EntityResponse;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,14 +22,105 @@ public class CandidatoServico {
 //  Implemente um sistema web que receba um JSON com os dados e mostre os seguintes resultados:
 //  • Quantos candidatos temos nessa lista em cada estado do Brasil?
 //  Map<Boolean, List<CandidatoDTO>> collect = candidatos.stream().collect(Collectors.groupingBy(i -> i.getEstado().equals(Estado.values().)));
-        qtdCandidatosPorEstados(candidatos);
+        Map<Integer, Estado> qtdCandidatosPorEstados = qtdCandidatosPorEstados(candidatos);
 //  • IMC médio em cada faixa de idade de dez em dez anos: 0 a 10; 11 a 20; 21 a 30, etc. (IMC = peso / altura^2)
+        float imc = 0;
+      for (int i = 0; i <= candidatos.size(); i++) {
+          int anoNascimento = LocalDate.parse(candidatos.get(i).getData_nasc()).getYear();
+          int anoAtual = LocalDate.now().getYear();
+          int idade = anoNascimento - anoAtual;
+          Map<Integer, Float> calcularIMC = calcularIMC(candidatos, i, idade);
+
 //  • Qual o percentual de obesos entre os homens e entre as mulheres? (É obeso quem tem IMC >30)
+          int qtdMasculino = 0, qtdFeminino = 0;
+          float percentualMasculino, percentualFeminino;
+
+          for (int a = 0; a <= calcularIMC.values().size(); a++) {
+              if (calcularIMC.get(a).floatValue() > 30) {
+                  if (candidatos.get(i).getSexo().equals("Feminino")) {
+                        qtdFeminino++;
+                  }
+                  if (candidatos.get(i).getSexo().equals("Masculino")) {
+                        qtdMasculino++;
+                  }
+              }
+          }
+          percentualFeminino = qtdFeminino / 100;
+          percentualMasculino = qtdMasculino / 100;
+      }
+
 //  • Qual a média de idade para cada tipo sanguíneo?
+
 //  • A quantidade de possíveis doadores para cada tipo sanguíneo receptor.
+
 //    ATENÇÃO: Somente pessoas com idade de 16 a 69 anos e com peso acima de 50 Kg podem doar sangue.
 
         return null;
+    }
+
+    private static Map<Integer, Float> calcularIMC(List<CandidatoDTO> candidatos, int index, int idade) {
+        float imc;
+        Map<Integer, Float> imcIdade = new HashMap<>();
+        if ( idade > 0 && idade <= 10 ) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if ( idade > 10 && idade <= 20) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if( idade > 20 && idade <= 30) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if( idade > 30 && idade <= 40) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if( idade > 40 && idade <= 50) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if( idade > 50 && idade <= 60) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if( idade > 60 && idade <= 70) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if( idade > 70 && idade <= 80) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if( idade > 80 && idade <= 90) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if( idade > 90 && idade <= 100) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        if( idade > 100 && idade <= 120) {
+            imc = formula(candidatos, index);
+            imcIdade.put(idade, imc);
+        }
+
+        return imcIdade;
+    }
+
+    private static float formula(List<CandidatoDTO> candidatos, int index) {
+        return candidatos.get(index).getPeso() / (candidatos.get(index).getAltura() * candidatos.get(index).getAltura());
     }
 
     private Map<Integer, Estado> qtdCandidatosPorEstados(List<CandidatoDTO> candidatos) {
